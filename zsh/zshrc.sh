1cee8333388d9cb2fe332bff84c10bd38733e80d
Niklas Reusch <nreusch@web.de>
2019-11-23 11:51:15 +0100

    added alias for codium, so it opens file and folder at the same time

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -10,6 +10,7 @@
 	alias v="vim -p"
 	mkdir -p /tmp/log
 	
+	alias codium="codium -a . -g" 	
 	# This is currently causing problems (fails when you run it anywhere that isn't a git project's root directory)
 	# alias vs="v `git status --porcelain | sed -ne 's/^ M //p'`"
 
